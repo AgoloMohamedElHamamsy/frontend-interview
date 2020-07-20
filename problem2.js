f4ae1b967258acee69d404453a5d1bd82419afd6
Abkreno <mohamedelhamamsy789@gmail.com>
2020-07-20 14:10:45 +0200

    Add 2 problems

---
+++ problem2.js
@@ -0,0 +1,6 @@
+/**
+  implement array.map, array.filter, array.reduce functions with the same exact output.
+  [1,2,3].map(i => i+1) -> [2,3,4]
+  [1,2,3].filter(i => i<=2) -> [3]
+  [1,2,3].reduce((accumulator, currentValue) => accumulator + currentValue) -> 10
+**/
